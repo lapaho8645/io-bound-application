a1a96995e1dc52199dc0bbbcf74428b1aecb99bc
lapaho8645 <lapaho8645@naver.com>
2022-12-19 17:19:52 +0900

    feat : 첫 페이지 캐싱

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -21,6 +21,9 @@ public class PostController {
     Producer producer;
     @Autowired
     ObjectMapper objectMapper;
+    @Autowired
+    PostCacheService postCacheService;
+
     // 1. 글을 작성한다.
     @PostMapping("/post")
     public Post createPost(@RequestBody Post post) throws JsonProcessingException {
@@ -32,20 +35,25 @@ public class PostController {
     // 2. 글 목록을 페이징하여 반환
     @GetMapping("/posts")
     public Page<Post> getPostList(@RequestParam(defaultValue = "1") Integer page) {
-        return postRepository.findAll(
-                PageRequest.of(page - 1, PAGE_SIZE, Sort.by("id").descending())
-        );
+        // 매번 요청이 들어올 때마다 새로운 쿼리를 날릴 필요가 없어진다. firstPostPage에 저장된 값을 사용자에게 준다.
+        if (page.equals(1)) {
+            return postCacheService.getFirstPostPage();
+        } else {
+            return postRepository.findAll(
+                    PageRequest.of(page - 1, PAGE_SIZE, Sort.by("id").descending())
+            );
+        }
     }
-    
+
     // 3. 글 번호로 조회
     @GetMapping("/post/{id}")
-    public Post getPostById(@PathVariable("id") Long id){
+    public Post getPostById(@PathVariable("id") Long id) {
         return postRepository.findById(id).get();
     }
-    
+
     // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
     @GetMapping("/search")
-    public List<Post> findPostsByContent(@RequestParam String content){
+    public List<Post> findPostsByContent(@RequestParam String content) {
         return postRepository.findByContentContains(content);
     }
 }
